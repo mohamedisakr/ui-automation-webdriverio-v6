6a78269bf9e7bbee227e1fb4cfeace38338c5bf1
mohamedisakr <mohamedisakr@gmail.com>
2020-12-02 07:31:33 +0200

    initial commit

---
+++ test/specs/basic.js
@@ -0,0 +1,99 @@
+/*
+ * 
+browser.url()
+browser.pause()
+browser.getTitle()
+
+$('locator').addValue()
+$('locator').getValue()
+$('locator').setValue()
+$('locator').click()
+$('locator').getAttribute()
+$('locator').getLocation()
+$('locator').getText()
+ */
+
+//
+describe("webdriver.io page", () => {
+  /*
+    //
+  xit("should have the right title", () => {
+    browser.url("https://webdriver.io");
+    expect(browser).toHaveTitle(
+      "WebdriverIO · Next-gen browser and mobile automation test framework for Node.js"
+    );
+  });
+
+  //
+  xit("should demonstrate the addValue & getValue", () => {
+    browser.url("https://webdriver.io");
+    let searchInput = $("#search_input_react");
+    searchInput.addValue("api");
+    let val = searchInput.getValue();
+    browser.pause(2000);
+    expect(val).toBe("api");
+  });
+
+  //
+  it("should demonstrate the click command", () => {
+    browser.url("https://webdriver.io");
+    const blogAnchor = $('[href="/blog/"]'); // $(".siteNavGroupActive > a"); //
+    blogAnchor.waitForDisplayed({ timeout: 17000 });
+    // browser.pause(7000);
+    blogAnchor.click();
+    // browser.pause(7000);
+  });
+  
+
+  //
+  it("should demonstrate getAttribute command", () => {
+    browser.url("https://webdriver.io");
+    let searchInput = $("#search_input_react");
+    let attrib = searchInput.getAttribute("title");
+    console.log("The title attribute is ", attrib);
+
+    searchInput.setValue("book");
+    attrib = searchInput.getAttribute("value");
+    console.log("The title attribute is ", attrib);
+  });
+
+
+  //
+  it("should demonstrate getLocation command", () => {
+    browser.url("https://webdriver.io");
+    let searchInput = $("#search_input_react");
+    let location = searchInput.getLocation();
+    console.log("The location is ", location);
+
+    const xLocation = searchInput.getLocation("x");
+    console.log("X location is ", xLocation);
+  });
+
+  
+
+  //
+  it("should demonstrate getText command", () => {
+    browser.url("https://webdriver.io");
+    const blogAnchor = $('[href="/blog/"]'); // $(".siteNavGroupActive > a"); //
+    console.log("The text value is ", blogAnchor.getText());
+  });
+
+  */
+});
+
+// const text = blogAnchor.getText();
+// blogAnchor.click();
+// expect(text).toBe("Blog");
+
+// browser.waitUntil(
+//   () => {
+//     return blogAnchor.isDisplayed();
+//   },
+//   {
+//     timeout: 15000,
+//     timeoutMsg: "expected text to be Blog after 15s",
+//   }
+// );
+
+// const isDisplayed = blogAnchor.isDisplayed();
+// expect(isDisplayed).toBe(true);
---
+++ test/specs/lesson09.js
@@ -0,0 +1,7 @@
+describe("Lesson 9 tasks", () => {
+  //
+  xit("Task 1: print page url in the console", () => {
+    browser.url("https://webdriver.io/docs/api.html");
+    console.log(browser.getUrl());
+  });
+});
