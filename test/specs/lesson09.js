448c681507ee7c618bcd072fe3984387345f460c
mohamedisakr <mohamedisakr@gmail.com>
2020-12-02 09:35:16 +0200

    using getText & getLocation methods

--- test/specs/lesson09.js
+++ test/specs/lesson09.js
@@ -4,4 +4,27 @@ describe("Lesson 9 tasks", () => {
     browser.url("https://webdriver.io/docs/api.html");
     console.log(browser.getUrl());
   });
+
+  // __docusaurus
+  xit("Task 2: print h1 in the console", () => {
+    browser.url("https://webdriver.io/docs/api.html");
+    const h1 = $("#__docusaurus");
+    console.log(h1.getText());
+  });
+
+  // <a href="https://github.com/SeleniumHQ/selenium/wiki/JsonWireProtocol">JSONWire protocol</a>
+  xit("Task 3: print h1 in the console", () => {
+    browser.url("https://webdriver.io/docs/api.html");
+    const label = $("//a[contains(.,'JSONWire')]");
+    console.log(label.getText());
+  });
+
+  //
+  it("Task 4: type some text in the search box", () => {
+    browser.url("https://webdriver.io/");
+    const searchBox = $("#search_input_react");
+    searchBox.setValue("course");
+    const val = searchBox.getValue();
+    console.log(val);
+  });
 });
